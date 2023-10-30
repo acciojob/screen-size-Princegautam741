c737c7f5dda2800d8bebaa95dbd96b88b1ceccf9
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-30 19:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// Function to update the width and height values
+function updateSizeInfo() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    // Update the content of the <span> elements inside <h1>
+    document.getElementById('width').textContent = width;
+    document.getElementById('height').textContent = height;
+}
+
+// Add an event listener to handle window resize
+window.addEventListener('resize', updateSizeInfo);
+
+// Initial update
+updateSizeInfo();
